f0ea371bcb88a768659ada49a23bc0111132cb52
ruiyunkoh <ruiyun.k@gmail.com>
2022-03-26 09:40:28 +0000

    set up database with db-migrate

--- routes/landing.js
+++ routes/landing.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.send("landing/index")
+    res.render("landing/index")
+})
+
+router.get('/contact', (req,res)=>{
+    res.render('landing/contact')
 })
 
 module.exports = router; 
