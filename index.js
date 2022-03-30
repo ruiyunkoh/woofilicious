33d483d98101a8a26842e71d820246c2c12d8739
ruiyunkoh <ruiyun.k@gmail.com>
2022-03-30 08:48:10 +0000

    tested registration and login. Made changes to errors

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
 //set up sessions
 app.use(session({
   store: new FileStore(),
-  secret: 'keyboard cat',
+  secret: process.env.SESSION_SECRET_KEY,
   resave: false,
   saveUninitialized: true
 }));
