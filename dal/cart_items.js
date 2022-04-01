50e420b1c023e68bc1c893b75e18ea3fc520bb2c
ruiyunkoh <ruiyun.k@gmail.com>
2022-04-01 03:22:51 +0000

    created dal for cart items

---
+++ dal/cart_items.js
@@ -0,0 +1,59 @@
+const { CartItem } = require('../models');
+
+const getCart = async (userId) => {
+    return await CartItem.collection()
+      .where({
+        'user_id': userId
+      }).fetch({
+        require: false,
+        withRelated: ['product', 'product.type']
+        });
+};
+
+const getCartItemByUserAndProduct = async (userId, productId) => {
+  return await CartItem.where({
+    'user_id': userId,
+    'product_id': productId
+  }).fetch({
+    require: false,
+    withRelated: ['product', 'product.type']
+    });
+};
+
+//add item to cart
+
+async function createCartItem(userId, productId, quantity) {
+  let cartItem = new CartItem({
+    'user_id': userId,
+    'product_id': productId,
+    'quantity': quantity
+  })
+  await cartItem.save();
+  return cartItem;
+};
+
+//remove item from cart
+
+async function removeFromCart(userId, productId) {
+  let cartItem = await getCartItemByUserAndProduct(userId, productId);
+  if (cartItem) {
+    await cartItem.destroy();
+    return true;
+  }
+  return false;
+};
+
+//update quantity of cart item
+
+async function updateQuantity(userId, productId, newQuantity) {
+  let cartItem = await getCartItemByUserAndProduct(userId, productId);
+  if (cartItem) {
+      cartItem.set('quantity', newQuantity);
+      cartItem.save();
+      return true;
+  }
+  return false;
+}
+
+
+module.exports = { getCart, getCartItemByUserAndProduct, createCartItem, removeFromCart, updateQuantity }
--- middlewares/index.js
+++ middlewares/index.js
@@ -7,7 +7,15 @@ const checkIfAuthenticated = (req, res, next) => {
   }
 }
 
+const checkIfLogin = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash("error_messages", "Please log in to proceed.");
+    res.redirect('/users/login');
+  }
+}
 
 module.exports = {
-  checkIfAuthenticated
+  checkIfAuthenticated, checkIfLogin
 }
